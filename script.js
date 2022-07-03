434c0106d49451ad66d63ef9b141e43dbf6d66bc
arsal2525 <arsalziakhan@gmail.com>
2022-07-02 20:30:10 -0700

    first commit

--- script.js
+++ script.js
@@ -45,3 +45,61 @@ function showSlides(n) {
 //   dots[slideIndex-1].className += " active";
 //   setTimeout(showSlides2, 2000); // Change image every 2 seconds
 // }
+
+// $(document).ready(function(){
+// $("#side-bars-icon").click(function () {
+//   $("#container-top-nav").toggleClass("mobile-container--hidden");
+// });
+// });
+
+
+// // trying stuff
+// HTML CSS JSResult Skip Results Iframe
+// EDIT ON
+// var words = ['Hi i like HTML', 'I also like css', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'sed do eiusmod tempor incididunt'],
+//     part,
+//     i = 0,
+//     offset = 0,
+//     len = words.length,
+//     forwards = true,
+//     skip_count = 0,
+//     skip_delay = 15,
+//     speed = 70;
+// var wordflick = function () {
+//   setInterval(function () {
+//     if (forwards) {
+//       if (offset >= words[i].length) {
+//         ++skip_count;
+//         if (skip_count == skip_delay) {
+//           forwards = false;
+//           skip_count = 0;
+//         }
+//       }
+//     }
+//     else {
+//       if (offset == 0) {
+//         forwards = true;
+//         i++;
+//         offset = 0;
+//         if (i >= len) {
+//           i = 0;
+//         }
+//       }
+//     }
+//     part = words[i].substr(0, offset);
+//     if (skip_count == 0) {
+//       if (forwards) {
+//         offset++;
+//       }
+//       else {
+//         offset--;
+//       }
+//     }
+//     $('.word').text(part);
+//   },speed);
+// };
+
+// $(document).ready(function () {
+//   wordflick();
+// });
+
